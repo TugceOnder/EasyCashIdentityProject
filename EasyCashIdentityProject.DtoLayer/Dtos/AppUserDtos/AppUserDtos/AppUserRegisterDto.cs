588d9fc1c40ba3522be188a957f3751502021532
TugceOnder <tugceonder775@gmail.com>
2025-02-12 00:44:01 +0300

    yeni özellikler eklendi register ve şifre confirmcode özelliği

--- EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserDtos/AppUserRegisterDto.cs
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserDtos/AppUserRegisterDto.cs
@@ -12,9 +12,9 @@ namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos.AppUserDtos
        // [Required(ErrorMessage = "Ad alanı zorunludur")]
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string EMail { get; set; }
+        public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string ConfirmPssword { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
--- EasyCashIdentityProject.PresantationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresantationLayer/Controllers/RegisterController.cs
@@ -27,19 +27,32 @@ namespace EasyCashIdentityProject.PresantationLayer.Controllers
         {
             if(ModelState.IsValid)
             {
+                Random random = new Random();       
                 AppUser appUser = new AppUser()
                 {
                     UserName = appUserRegisterDto.Username,
                     Name = appUserRegisterDto.Name,
                     Surname = appUserRegisterDto.Surname,
-                    Email = appUserRegisterDto.EMail,
-                  
-                };
+                    Email = appUserRegisterDto.Email,
+                    City="aaa",
+                    District="bbb",
+                    ImageUrl="cccc" ,// boş verlilmez uyarısan karşılık manuel olarak girdik 
+
+				  ConfirmCode = random.Next(100000, 1000000)
+			};
                 var result= await _userManager.CreateAsync(appUser,appUserRegisterDto.Password);
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index", "ConfirmMail");
                 }
+                  else
+					{
+						foreach (var item in result.Errors)
+						{
+							ModelState.AddModelError("", item.Description);
+						}
+					}
+				
        
             }
             return View();
