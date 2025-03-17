f250084cfbb1b531d0761de70eb4fedbf9b74a35
TugceOnder <tugceonder775@gmail.com>
2025-03-17 13:54:00 +0300

    customerAccount özellikler eklendi

--- EasyCashIdentityProject.PresantationLayer/Controllers/MyAccountsController.cs
+++ EasyCashIdentityProject.PresantationLayer/Controllers/MyAccountsController.cs
@@ -29,6 +29,29 @@ namespace EasyCashIdentityProject.PresantationLayer.Controllers
             appUserEditDto.ImageUrl = values.ImageUrl;
             return View(appUserEditDto);
         }
+        [HttpPost]
+        public async Task <IActionResult> Index(AppUserEditDto appUserEditDto) {
+            if (appUserEditDto.Password ==  appUserEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.PhoneNumber = appUserEditDto.PhoneNumber;
+                user.Surname = appUserEditDto.Surname;
+                user.City = appUserEditDto.City;
+                user.District = appUserEditDto.District;
+                user.Name = appUserEditDto.Name;
+                user.ImageUrl = "test";
+                user.Email = appUserEditDto.Email;
+                user.PasswordHash= _userManager.PasswordHasher.HashPassword(user,appUserEditDto.Password);  // veritabanıa şifreli pormatta yolluyor olacağı
+           var result = await _userManager.UpdateAsync(user);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+
+            
+            return View();
+        }
 
     }
 }
